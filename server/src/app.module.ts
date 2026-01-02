fbb7f022670308d8814b0fb80ec35c029f32ac0e
otsairaze <ssavelevdenchikk@gmail.com>
2026-01-02 01:35:40 +0100

    infra: create config root server & install prisma (#7)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
