b399da1c54e41e727a8a5da2ef559f7c38e191b6
otsairaze <ssavelevdenchikk@gmail.com>
2026-01-04 23:38:39 +0100

    Feature/main page (#13)
    * feat: made header and add particle
    
    * feat: made how section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Navigation } from '@/widgets/Navigation';
+import { Header } from '@/widgets/Header';
+import { StarsBackground } from '@/shared/lib/particles/particles';
+import { HowItWorks } from '@/widgets/HowItWorks';
 
 export default function Home() {
   return (
     <>
       <div>
+        <StarsBackground />
         <Navigation />
+        <Header />
+        <HowItWorks />
       </div>
     </>
   );
---
+++ client/src/entities/HowItWorksCard/model/types/cardList.ts
@@ -0,0 +1,10 @@
+import type { LucideIcon } from 'lucide-react';
+
+export interface CardListTypes {
+  title: string;
+  number: string;
+  firstImage: LucideIcon;
+  secondImage: LucideIcon;
+  titleCard: string;
+  description: string;
+}
